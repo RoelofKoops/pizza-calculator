c36bd5c91c41b57ac5c36fba7806c245ac79dd67
roel0Fzo <99055522@mydavinci.nl>
2019-09-11 07:12:48 +0200

    begonnen met pizza calculator

---
+++ pizzacalculator.js
@@ -0,0 +1,16 @@
+//Roelof Koops Opdracht:Pizza calculator
+
+
+document.write ('Aantal grote pizzas: ')
+var grote =(prompt ('Hoeveel grote pizzas wilt u bestellen? de grote pizza heeft een diameter van 35 centimeter'))
+document.write ( "<br/>")
+document.write ('Aantal gemiddelde pizzas: ')
+var gemiddelde =(prompt ('Hoeveel gemiddelde pizzas wilt u bestellen? de gemiddelde pizza heeft een diameter van 25 centimeter'));
+document.write ( "<br/>")
+document.write ('Aantal kleine pizzas: ')
+var kleine =(prompt('Hoeveel kleine pizzas wilt u bestellen? de kleine pizza heeft een diameter van 15 centimeter'))
+document.write ( "<br/>")
+document.write ('totaal prijs: €')
+document.write  (grote * 2)
+document.write (gemiddelde * 1.50)
+document.write (kleine * 1)
